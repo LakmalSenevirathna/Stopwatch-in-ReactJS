5cc17d737e53584459db44e637914b5fc321ebcf
Lakmal Senevirathna <laka.usjp@gmail.com>
2024-07-02 16:38:23 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [time,setTime] = useState(0);
+  const [running,setRunning] = useState(false);
+
+  useEffect(() => {
+    let interval;
+    if(running){
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 10 );
+      }, 10)
+    }else if (!running){
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [running]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1 className='header'>STOPWATCH</h1>
+      <div className='timer'>
+        <span>{("0" + Math.floor((time/60000) % 60)).slice(-2)} : </span>
+        <span>{("0" + Math.floor((time/1000) % 60)).slice(-2)} : </span>
+        <span>{("0" + Math.floor((time/10) % 100)).slice(-2)}</span>
+      </div>
+      <div className='buttons'>
+        <button onClick={() => setRunning(true)} >START</button>
+        <button onClick={() => setRunning(false)} >STOP</button>
+        <button onClick={() => setTime(0)} >RESET</button>
+      </div>
     </div>
   );
 }
